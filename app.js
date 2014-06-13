f6c0ed88c8cbd06b557f9c1d2e21036de34e35b4
Sushma Kodati <sushmakodati.95@gmail.com>
2014-06-13 17:17:56 +0530

    get request to recieve all trips created by userId

---
+++ app.js
@@ -0,0 +1,48 @@
+var restify=require('restify');
+var ip_addr='127.0.0.1';
+var port="4243";
+
+var server=restify.createServer({
+	name:"sbapp"
+});
+
+var mysql=require('mysql');
+var connection=mysql.createConnection({
+	host	: "localhost",
+	user	: "root",
+	password: "Tra@2014",
+	database: "sbtry",    
+});
+connection.connect(function(err){
+	if(err)
+		console.log("ERROR : "+err);
+	else
+		console.log("SUCCESS connected to database");
+});
+
+server.listen(port,ip_addr,function(){
+	console.log('%s listening on %s',server.name,server.url);
+});
+
+server.use(restify.queryParser());
+server.use(restify.bodyParser());
+server.use(restify.CORS());
+
+var PATH="/trips";
+server.post({path:PATH},getTrip);
+
+function getTrip (req,res,next) {
+	// body...
+	var data={};
+	console.log(req.params.userId);
+	data.userId=req.params.userId;
+	res.setHeader('Access-Control-Origin','*');
+	query=connection.query('select * from trip where createdBy='+data.userId,function(err,result){
+		if(err)
+			console.log("ERROR : "+err);
+		else
+			console.log("SUCCESS : "+result);
+	});
+	console.log(query.sql);
+	res.send(201);
+}
